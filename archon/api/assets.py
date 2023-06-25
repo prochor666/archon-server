4a71393413559c038dde69c09555da72592803d6
Jan Prochazka <jan.prochazka@softino.cz>
2023-06-26 00:25:19 +0200

    Parsing API requests all assets

--- archon/api/assets.py
+++ archon/api/assets.py
@@ -76,10 +76,13 @@ def _item_delete(data_pass: dict = {}) -> dict:
 def _servers(data_pass: dict = {}) -> dict:
     _filter = utils.ark('filter', data_pass)
     _sort = utils.ark('sort', data_pass)
-    data_pass = {
-        'filter': json.loads(_filter) if len(_filter) > 0 else {},
-        'sort': json.loads(_sort) if len(_sort) > 0 else {}
-    }
+    
+    if type(_filter) is str:
+        data_pass['filter'] =  json.loads(_filter) if len(_filter) > 0 else {}
+
+    if type(_sort) is str:
+        data_pass['sort'] = json.loads(_sort) if len(_sort) > 0 else {}
+        
     data_filter = utils.apply_filter(data_pass)
     u = servers.load(
         data_filter['filter'], data_filter['sort'], data_filter['exclude'])
@@ -137,10 +140,13 @@ def _server_delete(data_pass: dict = {}) -> dict:
 def _scripts(data_pass: dict = {}) -> dict:
     _filter = utils.ark('filter', data_pass)
     _sort = utils.ark('sort', data_pass)
-    data_pass = {
-        'filter': json.loads(_filter) if len(_filter) > 0 else {},
-        'sort': json.loads(_sort) if len(_sort) > 0 else {}
-    }
+
+    if type(_filter) is str:
+        data_pass['filter'] =  json.loads(_filter) if len(_filter) > 0 else {}
+
+    if type(_sort) is str:
+        data_pass['sort'] = json.loads(_sort) if len(_sort) > 0 else {}
+
     data_filter = utils.apply_filter(data_pass)
     u = scripts.load(
         data_filter['filter'], data_filter['sort'], data_filter['exclude'])
@@ -197,10 +203,13 @@ def _script_delete(data_pass: dict = {}) -> dict:
 def _sites(data_pass: dict = {}) -> dict:
     _filter = utils.ark('filter', data_pass)
     _sort = utils.ark('sort', data_pass)
-    data_pass = {
-        'filter': json.loads(_filter) if len(_filter) > 0 else {},
-        'sort': json.loads(_sort) if len(_sort) > 0 else {}
-    }
+
+    if type(_filter) is str:
+        data_pass['filter'] =  json.loads(_filter) if len(_filter) > 0 else {}
+
+    if type(_sort) is str:
+        data_pass['sort'] = json.loads(_sort) if len(_sort) > 0 else {}
+
     data_filter = utils.apply_filter(data_pass)
     u = sites.load(
         data_filter['filter'], data_filter['sort'], data_filter['exclude'])
